decf2abcba3c698bc8bdd652c354c4fb1b3dfd19
Jakub Wirwis <jakub.wirwis@tensquaregames.com>
2023-10-13 19:14:44 +0200

    new character + animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,37 +7,45 @@ public class PlayerMovement : MonoBehaviour
     public GameObject model;
     public Transform transform;
     public float runSpeed = 40f;
-   
+
     private float _horizontalMove;
     private bool _jump;
-    private AnimationController _animationController;
+    private Animator _animator;
 
     private void Awake()
     {
-        _animationController = model.GetComponent<AnimationController>();
+        _animator = model.GetComponent<Animator>();
     }
 
     private void Update()
     {
-        _horizontalMove =  Input.GetAxisRaw("Horizontal") * runSpeed;
+        _horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        _animator.SetBool("isMoving", _horizontalMove != 0);
 
         if (Input.GetButtonDown("Jump"))
         {
             _jump = true;
+            _animator.SetBool("isJumping", _jump);
         }
     }
-    
+
     private void FixedUpdate()
     {
         controller.Move(_horizontalMove * Time.fixedDeltaTime, _jump);
         _jump = false;
-        if (_horizontalMove == 0)
+        UpdateLocalScale();
+    }
+
+    private void UpdateLocalScale()
+    {
+        if (_horizontalMove != 0)
         {
-            _animationController.SetIdleState();
-            return;
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * (_horizontalMove > 0 ? 1 : -1), transform.localScale.y);
         }
-        
-        _animationController.SetWalkState();
-        transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * (_horizontalMove > 0 ? -1 : 1), transform.localScale.y);
+    }
+
+    public void OnLandEvent()
+    {
+        _animator.SetBool("isJumping", false);
     }
 }
